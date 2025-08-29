20c0df56163f63faf82c99542170b47dc55a1d64
dr-x <dr-x@mail.com>
2025-08-29 17:05:57 +0800

    fix: test controller

--- q-modules/q-system/src/main/java/com/drx/system/controller/TestController.java
+++ q-modules/q-system/src/main/java/com/drx/system/controller/TestController.java
@@ -1,16 +1,27 @@
 package com.drx.system.controller;
 
+import com.drx.base.entity.Result;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Slf4j
 @RestController
 @RequestMapping("/test")
 public class TestController {
 
+    @Value("${app:null}")
+    private String app;
+
+    @Value("${num:null}")
+    private String num;
+
     @GetMapping("")
-    public String test(){
-        return "test";
+    public Result<String> test() {
+        log.debug("test");
+        return Result.success(app + " and " + num);
     }
 
 }
